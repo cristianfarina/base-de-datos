8ce4bb3b82784721f9e364d63a556c97b26cd013
cristianfarina <118023322+cristianfarina@users.noreply.github.com>
2023-08-05 21:46:37 -0300

    corrección join_series_genres

--- Joins/join_series_genres.sql
+++ Joins/join_series_genres.sql
@@ -1,11 +1,10 @@
 /*Utilizando la base de datos de movies, queremos conocer, por un lado, los títulos
  y el nombre del género de todas las series de la base de datos.*/
-
 SELECT s.title, g.name 
 FROM series s, genres g
-where s.id = g.id;
+where s.genre_id = g.id;
 
 SELECT s.title, g.name 
 FROM series s
 join genres g
-on s.id = g.id;
+on s.genre_id = g.id;
