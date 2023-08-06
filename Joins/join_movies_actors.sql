9e886c71b8dd87c3db20a67c084a4509cff770e8
cristianfarina <118023322+cristianfarina@users.noreply.github.com>
2023-08-05 21:34:41 -0300

    corrección en Join movies_actors

--- Joins/join_movies_actors.sql
+++ Joins/join_movies_actors.sql
@@ -1,16 +1,13 @@
 /*Para nuestro próximo desafío, necesitamos obtener a todos los actores o actrices (mostrar nombre y apellido) 
 que han trabajado en cualquier película de la saga de La Guerra de las galaxias.*/
-select * 
-from movies m, actors a, actor_movie am
-
-select m.title as titulo_pelicula, a.first_name as nombre_actor, a.last_name as apellido_actor
-from movies m, actors a, actor_movie am
+select distinct a.first_name as nombre_actor, a.last_name as apellido_actor
+from movies m, actor_movie am, actors a
 where m.id = am.movie_id
-and a.id = am.movie_id
+and a.id = am.actor_id
 and m.title like "%galaxias%"
 
-select m.title as titulo_pelicula, a.first_name as nombre_actor, a.last_name as apellido_actor
-from movies m
-join actors a
-on m.id = a.favorite_movie_id
-and m.title like "%galaxias%"
+select distinct a.first_name as nombre_actor, a.last_name as apellido_actor
+from movies m 
+join actor_movie am on m.id = am.movie_id
+join actors a on a.id = am.actor_id
+where m.title like "%galaxias%"
